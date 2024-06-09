bf21923fb1934769686802ab3e0973a87d64fea6
Amit Mandal <138322339+amit-mandal-kgce@users.noreply.github.com>
2024-06-10 04:58:07 +0530

    core project add

--- src/component/HomeProjects.js
+++ src/component/HomeProjects.js
@@ -1,23 +1,32 @@
-import React from "react";
-import projectData from "../utils/projectData";
+import React, { useEffect, useState } from "react";
+import coreProects from "../utils/coreProjects";
+import { FaExternalLinkAlt } from "react-icons/fa";
+import Loader from "./Loader";
 
 const HomeProjects = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
   return (
     <>
-      <div className="w-full bg-slate-100 flex flex-col gap-4 p-5">
-        {projectData.map((element, index) => (
-          <div key={index} className="flex flex-row justify-around gap-4 p-5">
-            <div className="flex flex-col justify-between items-center ">
+      {loading ? <Loader/> : <div className="w-full bg-slate-100 flex flex-col gap-4 p-5">
+        {coreProects.map((element, index) => (
+          <div key={index} className="flex flex-row-reverse justify-around gap-8 p-5 shadow bg-dark6 text-white">
+            <div className="flex flex-col justify-around items-center ">
               <h1 className="text-xl sm:text-3xl md:text-5xl font-extrabold uppercase text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-pink-500">
                 {element.projectName}
               </h1>
               <p className="font-semibold py-4">{element.projectDetails}</p>
-              <div className="p-2 bg-gradient-to-r from-blue-400 via-red-400 to-green-400 rounded-full animate-rotate-left-to-right">
+              <div className="p-1 border-8 border-l-green-400 border-b-indigo-400 border-r-red-400 border-t-fuchsia-400 rounded-full animate-rotate-left-to-right">
                 <button
-                  onClick={element.handelLink}
-                  className="p-4 bg-green-800 text-xl rounded-full text-white animate-rotate-right-to-left"
+                  onClick={()=> window.open(`${element.linksProject}`, "_blank")}
+                  className="p-3 text-xl font-bold rounded-full text-white animate-rotate-right-to-left"
                 >
-                  GO
+                  <FaExternalLinkAlt/>
                 </button>
               </div>
             </div>
@@ -31,7 +40,7 @@ const HomeProjects = () => {
             </div>
           </div>
         ))}
-      </div>
+      </div>}
       <style>
         {`
         .animate-rotate-left-to-right {
---
+++ src/utils/coreProjects.js
@@ -0,0 +1,28 @@
+const coreProects = [
+  {
+    id: 1,
+    projectImg: "project2.png",
+    projectName: "INDUSTRIAL-PROJECTS",
+    linksProject: "https://github.com/amit-kgec-2024/INDUSTRIAL-PROJECTS",
+    projectDetails:
+      "Cycling is a green and healthy choice. This project aims to make the cycle a more accessible vehicle. The project thus aims to reintroduce the bicycle with more user friendly and low cost features. An attempt will be made to model a similar design for the simple bicycles.",
+  },
+  {
+    id: 2,
+    projectImg: "medicalimage.png",
+    projectName: "brain-stroke",
+    linksProject: "https://github.com/amit-kgec-2024/brain-stroke",
+    projectDetails:
+      "Medical image processing is a field of study that focuses on the application of image processing techniques to medical images. Medical images can include medical images such as X-rays, CT scans, and MRI scans, as well as microscopic images of cells and tissues. The goal of medical image processing is to improve the accuracy and efficiency of medical diagnosis and treatment.",
+  },
+  {
+    id: 3,
+    projectImg: "helmet1.png",
+    projectName: "Safety-helmet detection",
+    linksProject: "https://github.com/amit-kgec-2024/Safety-Helmet-Detection",
+    projectDetails:
+      "The primary aim of the project is to enhance construction site safety through the implementation of a robust Live Safety Helmet Detection system using CNN, Python, Keras, Tenserflow, OpenCV . The overarching goal is to leverage computer vision technology to automate the monitoring of safety helmet usage among construction site personnel.",
+  },
+];
+
+export default coreProects;
